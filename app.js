63f474b9950c5052b1085425e9406a117989d19a
yungi jung <hubiwibe@gmail.com>
2021-08-21 14:27:17 +0900

    🔥 Remove unnecessary code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import createError from 'http-errors';
 import express, { json, urlencoded } from 'express';
-import path, { join } from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
@@ -8,13 +7,11 @@ import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
 
 const app = express();
-const __dirname = path.resolve();
 
 app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
