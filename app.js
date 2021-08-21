3f7b28977099acc5565456462921f8d6308acffe
yungi jung <hubiwibe@gmail.com>
2021-08-21 14:38:07 +0900

    🌱 Modify error handler

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import createError from 'http-errors';
 import express, { json, urlencoded } from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
+import cors from 'cors';
 import helmet from 'helmet';
 
 const app = express();
@@ -20,19 +20,14 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  res.sendStatus(404);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.sendStatus(500);
 });
 
 export default app;
