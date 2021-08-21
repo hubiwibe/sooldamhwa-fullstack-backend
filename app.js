c997864006eb975b991f8c26a3e17bba61f5b103
yungi jung <hubiwibe@gmail.com>
2021-08-21 14:29:09 +0900

    🌱 Add cors and helmet

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import logger from 'morgan';
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
+import helmet from 'helmet';
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
+app.use(helmet());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
